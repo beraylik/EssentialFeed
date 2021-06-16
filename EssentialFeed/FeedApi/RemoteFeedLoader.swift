3ebcdd337d8ca5ac44b3495eb7ff6249e589baba
Genrikh Beraylik <beraylikgr@psbank.ru>
2021-06-16 17:10:28 +0300

    Added test for double load

--- EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -15,7 +15,6 @@ public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
     
-    
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
