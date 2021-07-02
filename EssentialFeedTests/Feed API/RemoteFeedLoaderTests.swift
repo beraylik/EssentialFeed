f2b4aad9fbb2ec0b04a8e51e2b567644b4984df6
Genrikh Beraylik <beraylikgr@psbank.ru>
2021-07-02 08:17:57 +0300

    Moved URLSession to prodution

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -124,8 +124,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         return .failure(error)
     }
     
-
-    
     private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
         
         let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
