f720aeead8978d8532d082775603c77c15c2216f
Genrikh Beraylik <beraylikgr@psbank.ru>
2021-07-28 16:31:30 +0300

    Does not delete cache upon creation

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  CacheFeedUsecase.swift
+//  EssentialFeedTests
+//
+//  Created by Genrikh Beraylik on 28.07.2021.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUserCaseTests: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+}
