d219cc59cd8a52b20b2ea0d3a55980eb357d21d4
Genrikh Beraylik <beraylikgr@psbank.ru>
2021-07-29 14:03:31 +0300

    Moved FeedStore to separate file

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Genrikh Beraylik on 29.07.2021.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
