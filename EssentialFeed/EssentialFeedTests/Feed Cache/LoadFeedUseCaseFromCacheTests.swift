f80651190971f80849cde9ca00fa07ca0da50ddd
Genrikh Beraylik <beraylikgr@psbank.ru>
2021-08-04 10:11:23 +0300

    Load command requests cache retrieval

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedUseCaseFromCacheTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedUseCaseFromCacheTests.swift
@@ -16,6 +16,15 @@ class LoadFeedUseCaseFromCacheTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [])
     }
  
+    func test_init_requestsCacheRetrieval() {
+        let (sut, store) = makeSUT()
+        
+        sut.load()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+ 
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
