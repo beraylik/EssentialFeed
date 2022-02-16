0251624866b0d1374c08455a31b51561db041bf6
Genrikh Beraylik <beraylikgr@psbank.ru>
2022-02-16 17:30:26 +0300

    Completed Test cases for SignInModel login method

--- EssentialFeed/EssentialFeediOS/Feed UI/Models/SignInModelTests.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Models/SignInModelTests.swift
@@ -15,6 +15,13 @@ final class SignInModelTests: XCTestCase {
     private var cacheSpy: CacheSpy?
     private var validatorSpy: ValidatorSpy?
     
+    override func tearDown() {
+        authServiceSpy = nil
+        cloudApiSpy = nil
+        cloudApiSpy = nil
+        cloudApiSpy = nil
+    }
+    
     func test_init_doesNothingUponCreation() {
         let _ = makeSUT()
         
@@ -22,6 +29,97 @@ final class SignInModelTests: XCTestCase {
         XCTAssertEqual(cloudApiSpy?.events, [])
         XCTAssertEqual(cacheSpy?.events, [])
         XCTAssertEqual(validatorSpy?.events, [])
+    }
+    
+    func test_login_responseNeedsConfirmationOnConfirmSignUp() {
+        let sut = makeSUT()
+        authServiceSpy?.loginResult = .success(.confirmSignUp)
+        
+        expect(sut, toCompleteLoginWith: .needsConfirmation)
+        
+        XCTAssertEqual(authServiceSpy?.events, [.login])
+    }
+    
+    func test_login_fetchErrorOnLoginError() {
+        let sut = makeSUT()
+        let error = SLError(userMessage: "an error")
+        authServiceSpy?.loginResult = .failure(error)
+        
+        expect(sut, toCompleteLoginWith: .error(title: "Login Failed", message: error.userMessage))
+        
+        XCTAssertEqual(authServiceSpy?.events, [.login])
+    }
+    
+    func test_login_fetchErrorOnSessionError() {
+        let sut = makeSUT()
+        let error = SLError(userMessage: "an error")
+        authServiceSpy?.loginResult = .success(.done)
+        authServiceSpy?.sessionResult = .failure(error)
+        
+        expect(sut, toCompleteLoginWith: .error(title: "Login Failed", message: error.userMessage))
+        
+        XCTAssertEqual(authServiceSpy?.events, [.login, .fetchAuthSession])
+    }
+    
+    func test_login_fetchErrorOnFetchingUserProfile() {
+        let sut = makeSUT()
+        let error = SLError(userMessage: "an error")
+        let session = AuthSession(idToken: "id", accessToken: "access", refreshToken: "refresh")
+        authServiceSpy?.loginResult = .success(.done)
+        authServiceSpy?.sessionResult = .success(session)
+        authServiceSpy?.userResult = .failure(error)
+        
+        expect(sut, toCompleteLoginWith: .error(title: "Login Failed", message: error.userMessage))
+        
+        XCTAssertEqual(authServiceSpy?.events, [.login, .fetchAuthSession, .fetchUserProfile])
+    }
+    
+    func test_login_fetchErrorOnGetAccountError() {
+        let sut = makeSUT()
+        let error = NSError(domain: "an error", code: 0)
+        let session = AuthSession(idToken: "id", accessToken: "access", refreshToken: "refresh")
+        let userProfile = UserProfile(firstName: "first", lastName: "last")
+        authServiceSpy?.loginResult = .success(.done)
+        authServiceSpy?.sessionResult = .success(session)
+        authServiceSpy?.userResult = .success(userProfile)
+        cloudApiSpy?.accountResult = .failure(error)
+        
+        expect(sut, toCompleteLoginWith: .error(title: "Login Again", message: "First time login"))
+        
+        XCTAssertEqual(authServiceSpy?.events, [.login, .fetchAuthSession, .fetchUserProfile])
+        XCTAssertEqual(cloudApiSpy?.events, [.getAccount])
+    }
+    
+    func test_login_showWelcomeScreenOnSuccessWithoutSkinType() {
+        let sut = makeSUT()
+        let session = AuthSession(idToken: "id", accessToken: "access", refreshToken: "refresh")
+        let userProfile = UserProfile(firstName: "first", lastName: "last")
+        let accountInfo = AccountInfo(askErythemaQuestion: true, lowDoseFlag: true, skinType: 0)
+        authServiceSpy?.loginResult = .success(.done)
+        authServiceSpy?.sessionResult = .success(session)
+        authServiceSpy?.userResult = .success(userProfile)
+        cloudApiSpy?.accountResult = .success(accountInfo)
+        
+        expect(sut, toCompleteLoginWith: .showWelcomeScreen)
+        
+        XCTAssertEqual(authServiceSpy?.events, [.login, .fetchAuthSession, .fetchUserProfile])
+        XCTAssertEqual(cloudApiSpy?.events, [.getAccount])
+    }
+    
+    func test_login_showHomeScreenOnSuccessWithSkinType() {
+        let sut = makeSUT()
+        let session = AuthSession(idToken: "id", accessToken: "access", refreshToken: "refresh")
+        let userProfile = UserProfile(firstName: "first", lastName: "last")
+        let accountInfo = AccountInfo(askErythemaQuestion: true, lowDoseFlag: true, skinType: 1)
+        authServiceSpy?.loginResult = .success(.done)
+        authServiceSpy?.sessionResult = .success(session)
+        authServiceSpy?.userResult = .success(userProfile)
+        cloudApiSpy?.accountResult = .success(accountInfo)
+        
+        expect(sut, toCompleteLoginWith: .showHomeScreen(token: "access", skinType: 1))
+        
+        XCTAssertEqual(authServiceSpy?.events, [.login, .fetchAuthSession, .fetchUserProfile])
+        XCTAssertEqual(cloudApiSpy?.events, [.getAccount])
     }
 
     // MARK: - Helpers
@@ -41,64 +139,92 @@ final class SignInModelTests: XCTestCase {
         return sut
     }
     
+    private func expect(_ sut: SignInModel, toCompleteLoginWith expectedResult: SignInResult, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.login(email: "", password: "") { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case (.needsConfirmation, .needsConfirmation):
+                break
+            case (.showWelcomeScreen, .showWelcomeScreen):
+                break
+            case let (.showHomeScreen(receivedToken, receivedSkinType), .showHomeScreen(expectedToken, expectedSkinType)):
+                XCTAssertEqual(receivedToken, expectedToken, file: file, line: line)
+                XCTAssertEqual(receivedSkinType, expectedSkinType, file: file, line: line)
+            case let (.error(receivedTitle, receivedMessage), .error(expectedTitle, expectedMessage)):
+                XCTAssertEqual(receivedTitle, expectedTitle, file: file, line: line)
+                XCTAssertEqual(receivedMessage, expectedMessage, file: file, line: line)
+            default:
+                XCTFail("Expected result got \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
     private class AuthServiceSpy: AuthService {
         enum Event: Equatable {
-
+            case login
+            case fetchAuthSession
+            case fetchUserProfile
         }
         
         private(set) var events = [Event]()
+        
+        var loginResult: LoginResult?
+        var sessionResult: SessionResult?
+        var userResult: UserResult?
         
         // MARK: AuthService
         
         func login(username: String, password: String, completion: @escaping LoginCompletion) {
-            
+            events.append(.login)
+            completion(loginResult!)
         }
         
         func fetchAuthSession(completion: @escaping AuthSessionCompletion) {
-            
+            events.append(.fetchAuthSession)
+            completion(sessionResult!)
         }
         
         func fetchUserProfile(completion: @escaping UserProfileCompletion) {
-            
+            events.append(.fetchUserProfile)
+            completion(userResult!)
         }
         
-        func resetPassword(username: String, completion: @escaping ResetCompletion) {
-            
-        }
-        
-        func logout(completion: @escaping LogoutCompletion) {
-            
-        }
+        // Unused
+        func resetPassword(username: String, completion: @escaping ResetCompletion) {}
+        func logout(completion: @escaping LogoutCompletion) {}
     }
     
     private class CloudApiSpy: CloudApi {
         enum Event: Equatable {
-
+            case getAccount
+            case postTreatment
         }
         
         private(set) var events = [Event]()
         
+        var accountResult: Result<AccountInfo, Error>?
+        var treatmentResult: Result<TreatmentInfo, Error>?
+        
         // MARK: CloudApi
         
         func getAccount(token: String, completion: @escaping (Result<AccountInfo, Error>) -> Void) {
-            
+            events.append(.getAccount)
+            completion(accountResult!)
         }
         
         func postTreatment(token: String, skinType: Int, completion: @escaping (Result<TreatmentInfo, Error>) -> Void) {
-            
+            events.append(.postTreatment)
+            completion(treatmentResult!)
         }
         
-        func putTreatmentSession(token: String, sessionID: Int, beginTherapyRequestData: Data, completion: @escaping (Result<TreatmentSessionInfo, Error>) -> Void) {
-            
-        }
-        
-        func postSystemStatus(token: String, sessionID: Int, state: TreatmentState, statusData: Data, completion: @escaping (Result<SystemStatusInfo, Error>) -> Void) {
-            
-        }
-        
-        func putTreatmentFollowup(token: String, sessionID: Int, rednessReported: Bool, completion: @escaping (Result<TreatmentFollowupInfo, Error>) -> Void) {
-            
-        }
+        // Unused
+        func putTreatmentSession(token: String, sessionID: Int, beginTherapyRequestData: Data, completion: @escaping (Result<TreatmentSessionInfo, Error>) -> Void) {}
+        func postSystemStatus(token: String, sessionID: Int, state: TreatmentState, statusData: Data, completion: @escaping (Result<SystemStatusInfo, Error>) -> Void) {}
+        func putTreatmentFollowup(token: String, sessionID: Int, rednessReported: Bool, completion: @escaping (Result<TreatmentFollowupInfo, Error>) -> Void) {}
     }
     
     private class CacheSpy: SignInCacheProtocol {
