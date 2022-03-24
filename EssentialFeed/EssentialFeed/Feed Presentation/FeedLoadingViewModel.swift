f065a5d136169c3f153482df24c9a8d69d20799d
Genrikh Beraylik <beraylikgr@psbank.ru>
2022-03-24 15:08:06 +0300

    Move FeedLoadingViewModel to a separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Genrikh Beraylik on 24.03.2022.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
