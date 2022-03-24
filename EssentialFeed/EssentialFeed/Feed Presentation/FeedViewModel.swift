ff371acbf9c82927ce5edcc9309a0a3884b53a00
Genrikh Beraylik <beraylikgr@psbank.ru>
2022-03-24 15:07:16 +0300

    Move FeedViewModel to a separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Genrikh Beraylik on 24.03.2022.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
