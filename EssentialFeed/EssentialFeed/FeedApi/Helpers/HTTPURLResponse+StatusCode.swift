a7e3c24d24a2fe5bf72ea9b2efe79070f3c6b993
Genrikh Beraylik <genrih95@yandex.ru>
2022-04-02 12:25:03 +0300

    Feature/image feed/remote image data loader (#5)
    * Set iOS 15 deployment target
    
    * Add load image data use cases in readme
    
    * Move FeedImageDataLoader to cross-platform framework
    
    * RemoteFeedImageDataLoader does not request urls on init
    
    * Rename anyURL helper method
    
    * `RemoteFeedImageDataLoader` requests url data on `loadImageData`
    
    * RemoteFeedImageDataLoader requests data from url twice from calling `loadImageData`
    
    * RemoteFeedImageDataLoader delivers error on request URL failure
    
    * RemoteFeedImageDataLoader.loadImageDataFromURL delivers invalid data error on non-200 HTTP response
    
    * RemoteFeedImageDataLoader delivers invalid data error on empty data result
    
    * RemoteFeedImageDataLoader.loadImageDataFromURL delivers non-empty received data on 200 HTTP response
    
    * RemoteFeedImageDataLoader.loadImageDataFromURL does not deliver result after instance has been deallocated
    
    * Add HTTPClientTask so clients can cancel HTTP requests
    
    * Extract duplicate error capturing logic by passing a taskHandler closure into the reusable `resultErrorFor` helper. Now, we can easily interact with the HTTP task as the request starts.
    
    * Synchronize access to global URLProtocolStub.stub with a private `DispatchQueue` to prevent data races
    
    * Remove default URLSessionHTTPClient initializer parameter
    
    * Cancelling the RemoteFeedImageDataLoader.loadImageDataFromURL cancels the HTTP client URL request
    
    * RemoteFeedImageDataLoader.loadImageDataFromURL does not deliver result after cancelling task
    
    * Moved `anyData` helper to a shared place
    
    * Extract duplicate HTTPClientSpy helper into a shared scope
    
    * Update mac os deployment target to 11.0
    
    * Move RemoteFeedImageDataLoader to production
    
    * Make RemoteFeedImageDataLoader conform FeedImageDataLoader protocol
    
    * RemoteFeedImageDataLoader.loadImageDataFromURL delivers connectivity error on HTTP client error
    
    * Refactor switch to mapError and flatMap
    
    * Remove @discardableResult attribute — FeedImageDataLoaderTask instances should be properly managed and never discarded.
    
    * Add RemoteFeedImageDataLoader end-to-end test to guarantee both the client (app) and server (backend) respects the API contract, so we can fetch image data from remote.
    
    * Extract duplicate base URL creation into a factory helper
    
    * Extract duplicate ephemeral HTTPClient creation into a factory helper
    
    * Extract duplicate HTTPURLResponse.statusCode == 200 validation into a `isOK` helper method
    
    * Move URLProtocolStub to a separate file
    
    * Rename test case class to reflect the use case it refers to

---
+++ EssentialFeed/EssentialFeed/FeedApi/Helpers/HTTPURLResponse+StatusCode.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPURLResponse+StatusCode.swift
+//  EssentialFeed
+//
+
+import Foundation
+
+extension HTTPURLResponse {
+    private static var OK_200: Int { return 200 }
+    
+    var isOK: Bool {
+        statusCode == HTTPURLResponse.OK_200
+    }
+}
